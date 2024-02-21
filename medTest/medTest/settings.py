5c6ad5a7223d50bd78e3fcf679cbc43fde2cf0b2
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2024-02-21 12:25:48 +0100

    Create models for db

--- medTest/medTest/settings.py
+++ medTest/medTest/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "quizz",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
--- medTest/quizz/models.py
+++ medTest/quizz/models.py
@@ -1,59 +1,99 @@
+import uuid
+import datetime
+
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-import datetime
 
 # Create your models here.
+class MedicalYear(models.Model):
+    MEDICAL_YEARS = [
+        ("1st" , "First year"),
+        ("2nd" , "Second Year"),
+        ("3rd" , "Third Year") ,
+        ("4th" , "Fourth Year"),
+        ("5th" , "Fifth Year") , 
+        ("6th" , "Sixth Year") ,
+        ("residency" , "Residency")
+    ]
 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    label = models.CharField(max_length = 9,choices=MEDICAL_YEARS)
 
 class Subject(models.Model):
-    # Medical years as choices
 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
+    medical_year = models.ForeignKey('MedicalYear', on_delete= models.CASCADE, 
+        related_name="subjects",
+        related_query_name="subject",
+         )
 
     def __str__(self) -> str:
         return self.name
 
+class Chapter(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length = 100 )
+    subject = models.ForeignKey('Subject' , on_delete = models.CASCADE,  
+        related_name="chapters",
+        related_query_name="chapter",
+    )
+    def __str__(self) -> str:
+        return self.name
 
 class Course(models.Model):
-    title = models.CharField(max_length=100)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
 
-    subject = models.ForeignKey(
-        Subject,
+    chapter = models.ForeignKey( 'Chapter',
         on_delete=models.CASCADE,
         related_name="courses",
         related_query_name="course",
     )
 
     def __str__(self) -> str:
-        return self.title
+        return self.name
+
+
+class ClinicalCase(models.Model):
+
+    scenario = models.TextField()
+
+    
+    course = models.ForeignKey(
+        'Course',
+        on_delete=models.CASCADE,
+        related_name="clinical_cases",
+        related_query_name="clinical_case",
+    )
+
+    
 
 
 class Question(models.Model):
-    # Choices question can of chapter Theory or Clinical Case
-    class MedSchoolYear(models.IntegerChoices):
-        FIRST_YEAR = 1, _("First year")
-        SECOND_YEAR = 2, _("Second Year")
-        THIRD_YEAR = 3, _("Third Year")
-        FOURTH_YEAR = 4, _("Fourth Year")
-        FIFTH_YEAR = 5, _("Sixth Year")
-        SIXTH_YEAR = 6, _("Seventh Year")
 
-    CALENDER_YEARS = [(y, y) for y in range(2000, datetime.datetime.now().year + 1)]
-
-    med_school_year = models.IntegerField(choices=MedSchoolYear)  # type: ignore
-    calender_year = models.IntegerField(choices=CALENDER_YEARS)
+    number = models.IntegerField(null=True, blank =True)
+    calender_year = models.IntegerField()
     content = models.TextField()
     course = models.ForeignKey(
-        Course,
+        'Course',
+        on_delete=models.CASCADE,
+        related_name="questions",
+        related_query_name="question",
+    )
+    clinical_case = models.ForeignKey(
+        'ClinicalCase',
         on_delete=models.CASCADE,
         related_name="questions",
         related_query_name="question",
     )
 
+
     def __str__(self) -> str:
-        top_str = self.content
-        return top_str
+        return self.content[0:10]
+
+
 
 
 class Choice(models.Model):
@@ -66,10 +106,9 @@ class Choice(models.Model):
         E = "e", _("E")
 
     content = models.CharField(max_length=250)
-    isCorrect = models.BooleanField()
     letter = models.CharField(max_length=1, choices=Letter)  # type: ignore
     question = models.ForeignKey(
-        Question,
+        'Question',
         on_delete=models.CASCADE,
         related_name="choices",
         related_query_name="choice",
@@ -77,3 +116,18 @@ class Choice(models.Model):
 
     def __str__(self) -> str:
         return self.content
+
+
+
+class Answer(models.Model):
+   
+   
+    #the answers will be for example a list of ["abc", "bd"] 
+    letters_combinations = models.CharField(max_length = 5)
+
+    question = models.ForeignKey(
+        'Question',
+        on_delete = models.CASCADE,
+        related_name = 'answers',
+        related_query_name = 'answer'
+    )
